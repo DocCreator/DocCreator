85cfb279212d80b0aa142d8ab8c4263ca3d11999
BorisMansencal <boris.mansencal@labri.fr>
2019-09-05 18:33:51 +0200

    remove qt include

--- framework/src/framework_global.h
+++ framework/src/framework_global.h
@@ -1,7 +1,7 @@
 #ifndef FRAMEWORK_GLOBAL_H
 #define FRAMEWORK_GLOBAL_H
 
-#include <QtCore/qglobal.h>
+//#include <QtCore/qglobal.h>
 //#if defined(FRAMEWORK_LIB)
 //#define FRAMEWORK_EXPORT Q_DECL_EXPORT
 //#else
