e0700c3bbbe68faa098c243e91e7db6b33cd022c
BorisMansencal <boris.mansencal@labri.fr>
2019-09-06 14:20:34 +0200

    checkEqual() should also work on OpenCV 2.4.x

--- tests/Degradations/testCommon.hpp
+++ tests/Degradations/testCommon.hpp
@@ -9,7 +9,7 @@ checkEqual(const cv::Mat &m1, const cv::Mat &m2)
 {
   return (m1.size() == m2.size()) &&
      (m1.type() == m2.type()) &&
-    cv::countNonZero( (m1!=m2) ) == 0;
+    (cv::sum( (m1!=m2) ) == cv::Scalar(0, 0, 0, 0));
 }
 
 
