7d5c8eb466ffd94ae74e3459315fd526a7a5d6df
BorisMansencal <boris.mansencal@labri.fr>
2020-07-08 17:46:14 +0200

    Add NoiseDegradation

--- framework/src/Degradations/GradientDomainDegradation.cpp
+++ framework/src/Degradations/GradientDomainDegradation.cpp
@@ -9,6 +9,7 @@
 #include <opencv2/highgui/highgui.hpp>
 
 #include "FileUtils.hpp"
+#include "ColorUtils.hpp" //dc::isGray()
 
 namespace dc {
   namespace GradientDomainDegradation {
@@ -167,58 +168,6 @@ copyOnto(const cv::Mat &stainImg,
 }
 
 
-/*
-  Tells whether the provided image @a in is gray.
- */
-bool
-isGray(const cv::Mat &in)
-{
-  assert(in.type() == CV_8UC1 ||
-	 in.type() == CV_8UC3 ||
-	 in.type() == CV_8UC4);
-  
-  if (in.type() == CV_8UC1) {
-    return true;
-  }
-  if (in.type() == CV_8UC3) {
-    int rows = in.rows;
-    int cols = in.cols;
-    if (in.isContinuous()) {
-      cols *= rows;
-      rows = 1;
-    }
-    for (int i=0; i<rows; ++i) {
-      const cv::Vec3b *p = in.ptr<cv::Vec3b>(0);
-      for (int j=0; j<cols; ++j) {
-	const cv::Vec3b &pj = p[j];
-	if (pj[0] != pj[1] || pj[0] != pj[2])
-	  return false;
-      }
-    }
-    return true;
-  }
-  
-  if (in.type() == CV_8UC4) {
-    int rows = in.rows;
-    int cols = in.cols;
-    if (in.isContinuous()) {
-      cols *= rows;
-      rows = 1;
-    }
-    for (int i=0; i<rows; ++i) {
-      const cv::Vec4b *p = in.ptr<cv::Vec4b>(0);
-      for (int j=0; j<cols; ++j) {
-	const cv::Vec4b &pj = p[j];
-	if (pj[0] != pj[1] || pj[0] != pj[2])
-	  return false;
-      }
-    }
-    return true;
-  }
-
-  return false;
-}
-
 cv::Mat
 degradation(const cv::Mat &in,
 	    const std::string &stainDirName,
@@ -269,7 +218,7 @@ degradation(const cv::Mat &in,
 
   bool isImgGray = false;
   if (insertType == InsertType::INSERT_AS_GRAY_IF_GRAY) {
-    isImgGray = isGray(in);
+    isImgGray = dc::isGray(in);
   }
   const bool haveToConvert = (insertType == InsertType::INSERT_AS_GRAY
 			      || (insertType == InsertType::INSERT_AS_GRAY_IF_GRAY && isImgGray));
