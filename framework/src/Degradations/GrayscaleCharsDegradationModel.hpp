c2388c094efe50728e1479a543e4c2605b814937
BorisMansencal <boris.mansencal@labri.fr>
2021-04-20 11:39:05 +0200

    update doc

--- framework/src/Degradations/GrayscaleCharsDegradationModel.hpp
+++ framework/src/Degradations/GrayscaleCharsDegradationModel.hpp
@@ -22,28 +22,29 @@ namespace dc {
      In : 2nd International Workshop on Historical Document Imaging and Processing. 2013.
 
 
-     We propose a local noise model for GRAYSCALE images. Its main principle is to locally degrade the image in the
-     neighbourhoods of “seed-points” selected close to the character boundary. These points define the center of
-     “noise regions”. The pixel values inside the noise region are modified by a Gaussian random distribution
-     to make the final result more realistic. While Kanungo noise models scanning artifacts, our model simulates
-     degradations due to the age of the document itself and printing/writing process such as ink splotches, white
-     specks or streaks. It is very easy for users to parameterize and create a set of benchmark databases with
-     an increasing level of noise. These databases will further be used to test the robustness of different grayscale
-     document image analysis methods (i.e. text line segmentation, OCR, handwriting recognition).
+     We propose a local noise model for GRAYSCALE images. Its main principle is to locally degrade
+     the image in the neighbourhoods of “seed-points” selected close to the character boundary.
+     These points define the center of “noise regions”. The pixel values inside the noise region
+     are modified by a Gaussian random distribution to make the final result more realistic.
+     While Kanungo noise models scanning artifacts, our model simulates degradations due to the age
+     of the document itself and printing/writing process such as ink splotches, white specks or
+     streaks.
 
-     There are 3 kinds of degradation spots: independent spots, overlapping spots and disconnection spots.
+     There are 3 kinds of degradation spots: independent spots, overlapping spots and disconnection
+     spots.
      Ratio of disconnection spots will be determined as (1.0 - @a indepentSpotsRatio - @a overlappingSpotsRatio).
 
      @a img must be of type CV_8UC1, CV_8UC3 or CV_8UC4.
      Output image will be of the same type.
 
-     @param level quantity of degradation, in [0; 10]. The number of degradation spots will be determined according to number of connected components in image. The higher the level, the higher the number of degradation spots.
+     @param level scale factor, in [0; 10]. The number of degradation spots will be determined according to the number of connected components in image and multiplied by this scale factor. The higher the level, the higher the number of degradation spots.
      @param indepentSpotsRatio ratio of independent spots, in [0.0; 1.0]
      @param overlappingSpotsRatio ratio of overlapping spots, in [0.0; 1.0]
 
   */
     extern FRAMEWORK_EXPORT cv::Mat degradation(const cv::Mat &img, int level = 1,
-						float indepentSpotsRatio = 0.33f, float overlappingSpotsRatio = 0.33);
+						float indepentSpotsRatio = 0.33f,
+						float overlappingSpotsRatio = 0.33);
 
   } //namespace GrayscaleCharsDegradation
 
